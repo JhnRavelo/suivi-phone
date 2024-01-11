8d536375215d7eff7944a109e8d8391dee401286
JhnRavelo <johnravelo135@gmail.com>
2024-01-11 09:31:21 +0300

    style dropDownTypeList

--- components/DropDownTypeLists.js
+++ components/DropDownTypeLists.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Dropdown } from 'react-native-element-dropdown';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import styles from "../styles/dropDownListStyles"
-import useQRCodeGenerator from '../hooks/useQRCodeGenerator';
+import React, { useState } from "react";
+import { Image, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import styles from "../styles/dropDownListStyles";
+import useQRCodeGenerator from "../hooks/useQRCodeGenerator";
+import CharpentIcon from "../assets/png/charpenterie.png";
 
 const DropDownTypeLists = () => {
   const [value, setValue] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
-  const {productTypes} = useQRCodeGenerator()
+  const { productTypes } = useQRCodeGenerator();
 
   const renderLabel = () => {
     if (value || isFocus) {
       return (
-        <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+        <Text style={[styles.label, isFocus && { color: "blue" }]}>
           Type de ménuiserie
         </Text>
       );
@@ -25,7 +25,7 @@ const DropDownTypeLists = () => {
     <View style={styles.container}>
       {renderLabel()}
       <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
         placeholderStyle={styles.placeholderStyle}
         selectedTextStyle={styles.selectedTextStyle}
         inputSearchStyle={styles.inputSearchStyle}
@@ -36,23 +36,26 @@ const DropDownTypeLists = () => {
         maxHeight={300}
         labelField="label"
         valueField="value"
-        placeholder={!isFocus ? 'Sélectionnez le type de menuiserie' : '...'}
+        placeholder={!isFocus ? "Sélectionnez le type de menuiserie" : "..."}
         searchPlaceholder="Search..."
         value={value}
         onFocus={() => setIsFocus(true)}
         onBlur={() => setIsFocus(false)}
-        onChange={item => {
+        onChange={(item) => {
           setValue(item.value);
           setIsFocus(false);
         }}
-        // renderLeftIcon={() => (
-        //   <AntDesign
-        //     style={styles.icon}
-        //     color={isFocus ? 'blue' : '#A2A2A2'}
-        //     name="Safety"
-        //     size={20}
-        //   />
-        // )}
+        renderLeftIcon={() => (
+          <View style={styles.leftIconView}>
+            <Image
+              source={CharpentIcon}
+              style={[
+                styles.iconStyle,
+                { tintColor: `${isFocus ? "blue" : "black"}` },
+              ]}
+            />
+          </View>
+        )}
       />
     </View>
   );
--- styles/dropDownListStyles.js
+++ styles/dropDownListStyles.js
@@ -1,55 +1,61 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React from "react";
+import { StyleSheet } from "react-native";
 
 const dropDownListStyles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        padding: 16,
-        marginBottom: -14,
-      },
-      dropdown: {
-        height: 40,
-        borderColor: '#A2A2A2',
-        borderWidth: 0.5,
-        borderRadius: 8,
-        paddingHorizontal: 0,
-      },
-      icon: {
-        marginRight: 5,
-      },
-      label: {
-        position: 'absolute',
-        backgroundColor: 'white',
-        left: 22,
-        top: 8,
-        zIndex: 999,
-        paddingHorizontal: 0,
-        fontSize: 12,
-      },
-      placeholderStyle: {
-        fontSize: 14,
-      },
-      selectedTextStyle: {
-        marginLeft: 26,
-        fontSize: 14,
-        color: "#A2A2A2",
-        fontWeight: "700"
-      },
-      iconStyle: {
-        width: 20,
-        height: 20,
-      },
-      inputSearchStyle: {
-        height: 40,
-        fontSize: 14,
-      },
-      itemTextStyle: {
-        fontSize: 14,
-        paddingTop: 5,
-        paddingBottom: 5,
-        marginBottom: -20,
-        marginTop: -20
-      }
-})
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+    marginBottom: -14,
+  },
+  dropdown: {
+    height: 40,
+    borderColor: "#A2A2A2",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 0,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 0,
+    fontSize: 12,
+  },
+  placeholderStyle: {
+    fontSize: 14,
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+    color: "#A2A2A2",
+    fontWeight: "700",
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 14,
+  },
+  itemTextStyle: {
+    fontSize: 14,
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginBottom: -20,
+    marginTop: -20,
+  },
+  leftIconView: {
+    width: "auto",
+    height: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+});
 
-export default dropDownListStyles
+export default dropDownListStyles;

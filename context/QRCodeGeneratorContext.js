7a3d47f593333959ccf5ddcda249be92fc76c8e1
JhnRavelo <johnravelo135@gmail.com>
2024-01-11 08:28:41 +0300

    Add File QRCodeGeneratorContext

---
+++ context/QRCodeGeneratorContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const QRCodeGeneratorContext = createContext()
+
+const QRCodeGeneratorProvider = ({children}) => {
+    const [productTypes, setProductTypes] = useState()
+    return(
+        <QRCodeGeneratorContext.Provider value={{productTypes, setProductTypes}} >
+            {children}
+        </QRCodeGeneratorContext.Provider>
+    )
+}
+
+export {QRCodeGeneratorProvider}
+
+export default QRCodeGeneratorContext

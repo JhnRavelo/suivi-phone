5f524c77143be19ef8cea697de61a740e14bb1e5
JhnRavelo <johnravelo135@gmail.com>
2024-01-19 14:19:55 +0300

    add Button components props style

--- components/Button.js
+++ components/Button.js
@@ -4,14 +4,14 @@ import useButtonStyles from "../styles/buttonStyles";
 import useHeaderStyles from "../styles/header";
 import useScanStyles from "../styles/scanStyles"
 
-const AppButton = ({ text, onPress, style }) => {
+const AppButton = ({ text, onPress, style, viewStyle, textStyle }) => {
   const headerStyles = useHeaderStyles()
   const scanStyles = useScanStyles()
   const buttonStyles = useButtonStyles()
   return (
     <TouchableOpacity style={[buttonStyles.buttonContainer, style]} onPress={()=>onPress()} >
-      <View style={[headerStyles.logoutButtonView, scanStyles.scanButtonView, buttonStyles.buttonView]} >
-        <Text style={[headerStyles.logoutButtonText, buttonStyles.buttonText]}>{text}</Text>
+      <View style={[headerStyles.logoutButtonView, scanStyles.scanButtonView, buttonStyles.buttonView, viewStyle]} >
+        <Text style={[headerStyles.logoutButtonText, buttonStyles.buttonText, textStyle]}>{text}</Text>
       </View>
     </TouchableOpacity>
   );

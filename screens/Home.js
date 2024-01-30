dc99497122346d810cc9cadcb9b99f9b6e1489fa
JhnRavelo <johnravelo135@gmail.com>
2024-01-30 11:47:42 +0300

    move hasLocationAdress from Home to QrCode

--- screens/Home.js
+++ screens/Home.js
@@ -1,40 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavigationBottomTabHome from "../navigation/NavigationBottomTabHome";
 import { useLoading } from "../hooks/useLoading";
 import CircleLoading from "../components/CircleLoading";
-import * as Location from "expo-location"
 
 const Home = () => {
-  const {loading} = useLoading()
-  const [adress, setAdress] = useState(null)
-
-  useEffect(()=>{
-    hasLocationAdress()
-  }, [])
-
-const hasLocationAdress = async ()=>{
-  const {status} = await Location.requestForegroundPermissionsAsync()
-
-  if(status!=="granted"){
-    alert("Vous n'avez pas de permission sur la localiastion")
-    return
-  }
-  let network = await Location.enableNetworkProviderAsync()
-  console.log(network)
-  let currentLocation = await Location.getCurrentPositionAsync({})
-  let currentAdress = await Location.reverseGeocodeAsync({
-    longitude: currentLocation.coords.longitude,
-    latitude: currentLocation.coords.latitude
-  })
-  setAdress(currentAdress)
-
-  console.log("ADRESSE", currentAdress)
-}
+  const { loading } = useLoading();
 
   return (
     <>
       <NavigationBottomTabHome />
-      {loading && <CircleLoading /> }
+      {loading && <CircleLoading />}
     </>
   );
 };
--- screens/QrCode.js
+++ screens/QrCode.js
@@ -4,16 +4,20 @@ import NavigationStackQRCodeGenerator from "../navigation/NavigationStackQRCodeG
 import useScreen from "../hooks/useScreen";
 import useAxiosPrivate from "../hooks/usePrivateAxios";
 import useQRCodeGenerator from "../hooks/useQRCodeForm";
+import useLocation from "../hooks/useLocation";
+import * as Location from "expo-location";
 
 const QrCode = ({ navigation }) => {
   const axiosPrivate = useAxiosPrivate();
   const screenRoute = useRoute();
   const { setScreen } = useScreen();
   const { setProductTypes } = useQRCodeGenerator();
+  const { setLocation } = useLocation();
   let screenName = screenRoute.name;
 
   useEffect(() => {
     fetchTypesOfProducts();
+    hasLocationAdress()
   }, []);
 
   useEffect(() => {
@@ -32,6 +36,23 @@ const QrCode = ({ navigation }) => {
       }
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const hasLocationAdress = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Vous n'avez pas de permission sur la localiastion");
+    } else {
+      await Location.enableNetworkProviderAsync();
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      let currentAddress = await Location.reverseGeocodeAsync({
+        longitude: currentLocation.coords.longitude,
+        latitude: currentLocation.coords.latitude,
+      });
+      let address = currentAddress[0];
+      console.log(address.city)
+      setLocation(`${address.country}-${address.city}-${address.region}`);
     }
   };
 

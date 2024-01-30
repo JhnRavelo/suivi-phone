4f05b9b93a6f9e2bcfc674c452d3fbc77ce60b93
JhnRavelo <johnravelo135@gmail.com>
2024-01-30 09:30:55 +0300

    add screen home hasLocationAdress

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBottomTabHome from "../navigation/NavigationBottomTabHome";
 import { useLoading } from "../hooks/useLoading";
 import CircleLoading from "../components/CircleLoading";
+import * as Location from "expo-location"
 
 const Home = () => {
   const {loading} = useLoading()
+  const [adress, setAdress] = useState(null)
+
   useEffect(()=>{
-    console.log(loading)
-  }, [loading])
+    hasLocationAdress()
+  }, [])
+
+const hasLocationAdress = async ()=>{
+  const {status} = await Location.requestForegroundPermissionsAsync()
+
+  if(status!=="granted"){
+    alert("Vous n'avez pas de permission sur la localiastion")
+    return
+  }
+  let network = await Location.enableNetworkProviderAsync()
+  console.log(network)
+  let currentLocation = await Location.getCurrentPositionAsync({})
+  let currentAdress = await Location.reverseGeocodeAsync({
+    longitude: currentLocation.coords.longitude,
+    latitude: currentLocation.coords.latitude
+  })
+  setAdress(currentAdress)
+
+  console.log("ADRESSE", currentAdress)
+}
+
   return (
     <>
       <NavigationBottomTabHome />

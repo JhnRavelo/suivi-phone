f4b824d45ef1c65d251b410a5927a749496d4203
JhnRavelo <johnravelo135@gmail.com>
2024-03-11 16:34:45 +0300

    create headerArray asset

---
+++ assets/js/dataSuivi.js
@@ -0,0 +1,8 @@
+export const headerArray = [
+    {label: "Problème", width: 150},
+    {label: "Solution", width: 150},
+    {label: "Observation", width: 180},
+    {label: "Technicien", width: 90},
+    {label: "Date", width: 100},
+    {label: "Action", width: 60},
+]

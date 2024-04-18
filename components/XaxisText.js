034ae1812a64759c59b89c0f7c2971e8e6bdeded
JhnRavelo <johnravelo135@gmail.com>
2024-04-18 11:20:00 +0300

    chore: create xaxistext component

---
+++ components/XaxisText.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, useFont } from "@shopify/react-native-skia";
+import { useDerivedValue, withTiming } from "react-native-reanimated";
+
+const XAxisText = ({ x, y, text, selectedBar }) => {
+  const font = useFont(require("../assets/fonts/Lato-Regular.ttf"), 14);
+
+  const color = useDerivedValue(() => {
+    if (selectedBar.value === text) {
+      return withTiming("#1C2B39");
+    } else if (selectedBar.value === null) {
+      return withTiming("#1C2B39");
+    } else {
+      return withTiming("#d1d0c5");
+    }
+  });
+
+  if (!font) {
+    return null;
+  }
+  const fontSize = font?.getMetrics(text);
+
+  return (
+    <Text
+      font={font}
+      x={x - fontSize?.bounds?.width / 2}
+      y={y}
+      text={text}
+      color={color}
+    />
+  );
+};
+
+export default XAxisText;
